70453db7c16de6508f9b208d0976335d50c78054
Gary <gary@gwonggg.com>
2013-10-11 20:20:04 -0400

    Add homework files
    Add source code for homework 1

---
+++ src/utils/linearly-separable-points.cc
@@ -0,0 +1,7 @@
+// Copyright 2013 Gary Wong
+#include "src/utils/linearly-separable-points.h"
+#include <cstdlib>
+LinearlySeparablePoints::LinearlySeparablePoints() {
+  unsigned seed;
+  rand_r(&seed);
+}
---
+++ src/utils/linearly-separable-points.h
@@ -0,0 +1,11 @@
+// Copyright 2013 Gary Wong
+
+#ifndef SRC_UTILS_LINEARLY_SEPARABLE_POINTS_H_
+#define SRC_UTILS_LINEARLY_SEPARABLE_POINTS_H_
+
+class LinearlySeparablePoints {
+  public:
+    LinearlySeparablePoints();
+};
+
+#endif  // SRC_UTILS_LINEARLY_SEPARABLE_POINTS_H_
